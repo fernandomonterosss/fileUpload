2dc73c35ea592db34bae56ad2a43f3b4d3346a86
Fernando Monteros <montefer13@gmail.com>
2019-03-27 14:17:57 -0600

    href

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { FotosComponent } from './components/fotos/fotos.component';
 import { CargaComponent } from './components/carga/carga.component';
 
 
-const RUTAS: Routes = [
+const ROUTES: Routes = [
     { path: 'fotos', component: FotosComponent },
     { path: 'carga', component: CargaComponent },
-    { path: '**', pathMatch:'full', redirectTo: 'fotos' }
+    { path: '**', pathMatch:'full', redirectTo: 'fotos' },
+    { path: '', pathMatch: 'full', redirectTo: 'fotos' },
 ];
 
-export const APP_ROUTES = RouterModule.forRoot( RUTAS );
+export const APP_ROUTES = RouterModule.forRoot( ROUTES );
